255663d638de4df1f36c8c58e4d5d7b79c9f829b
Adam Espinosa <espiadam@github.com>
2018-02-03 17:36:00 -0600

    Stack.h done

---
+++ Stack.h
@@ -0,0 +1,20 @@
+
+struct StackNode
+{
+    char c;
+    int data1;
+    int data2;
+    StackNode* next;
+};
+
+class Stack
+{
+private:
+    StackNode* first;
+public:
+    void push(int x);
+    int pop();
+    bool isEmpty();
+    Stack();
+
+};
